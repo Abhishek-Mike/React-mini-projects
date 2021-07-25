4b302ad8b7195353c80b757957c6bbd7c7005aa0
Abhishek Sadhu <abhisadhu.007@gmail.com>
2021-07-25 23:53:38 +0530

    counter-app

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,28 +6,27 @@ class Counter extends Component {
     tags: ["tag1", "tag2", "tag3"],
   };
 
-// if not using arrow function use constructors
-//   constructor() {
-//     super();   // you have to call super for parent class inside child(here)
-//     this.handleIncrement = this.handleIncrement.bind(this);
-//   }
+  // if not using arrow function use constructors
+  //   constructor() {
+  //     super();   // you have to call super for parent class inside child(here)
+  //     this.handleIncrement = this.handleIncrement.bind(this);
+  //   }
 
   handleIncrement = () => {
     //console.log("Increment Clicked!!", this);
     //console.log(product);
-    this.setState({ count: this.state.count + 1})
+    this.setState({ count: this.state.count + 1 });
     //1. state of this component is going to change
     //2. react will schedule a call to render method
     //3. return new react element has two children ( span & button)
   };
-
 
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={ () => this.handleIncrement() }
+          onClick={() => this.handleIncrement()}
           className="btn btn-secondary btn-sm"
         >
           Increment
